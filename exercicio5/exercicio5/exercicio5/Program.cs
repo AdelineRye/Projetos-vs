49f61154a6cc3e52fffca14599414e0cc96254d3
2023102020023 <2023102020023@JIPA-LABIN01-08>
2023-03-02 14:03:06 -0400

    02/03/2023

---
+++ exercicio5/exercicio5/exercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class exercicio5
+{
+    static void Main()
+    {
+        Console.WriteLine("===SALARIO ATUAL E SEU PORCENTUAL===");
+
+        Console.Write("Digite o seu salário atual: ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("\nDigite quanto você receberá de aumento: ");
+        double porcentagemAumento = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = ((porcentagemAumento * salario) / 100) + salario;
+        Console.WriteLine("\nNovo salário = R$ " + resultado);
+        Console.ReadKey();
+
+    }
+
+
+}
