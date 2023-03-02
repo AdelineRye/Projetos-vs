4ed67fb7a3f02a0c2f4fe2557bfded45a41e48e2
2023102020023 <2023102020023@JP-BIBPESQ-08>
2023-03-02 19:18:05 -0400

    02/03/23

---
+++ estudar e lembrar/estudos/estudos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class estudooff
+{
+   static void Main()
+    {
+        Console.Write("Digite um número interito: ");
+        double numero = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite um número inteiro: ");
+        double numero2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = (numero + numero2) / 2;
+
+        Console.WriteLine("resultado: = " + resultado);
+        Console.ReadKey();
+
+
+    }
+
+
+}
--- exercicio4/exercicio4/exercicio4/Program.cs
+++ exercicio4/exercicio4/exercicio4/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+class exercicio4
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        int resultado = (idade * 365);
+
+        Console.WriteLine("resultado = " + resultado);
+        Console.ReadKey();
+    }
+
+}
